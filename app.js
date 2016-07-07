a1b2328dfc6f9241f98d9c95e76ecd526e572bb9
John Tucker <john@larkintuckerllc.com>
2016-07-07 09:11:35 -0400

    optional Leap Motion integration

--- app.js
+++ app.js
@@ -6,8 +6,14 @@
   var REPO = 'ds-example';
   var CONFIG_FILENAME = 'config.json';
   var PDF_FILENAME = 'example.pdf';
+  var SWIPE_LEFT = 0;
+  var SWIPE_RIGHT = 1;
+  var SWIPE_UP = 2;
+  var SWIPE_DOWN = 3;
   var ds = window.ds;
   var thr0w = window.thr0w;
+  // OPTIONAL LEAP MOTION INTEGRATION
+  var Leap = window.Leap;
   document.addEventListener('DOMContentLoaded', ready);
   function ready() {
     var currentPage = 1;
@@ -15,9 +21,42 @@
     var pdf;
     var loaded = false;
     var frameEl = document.getElementById('my_frame');
+    var leapController = Leap.loop({enableGestures: true}, leapCallback);
+    var lastGesture = Date.now();
+    leapController.on('gesture', handleGesture);
     thr0w.setBase(BASE);
     thr0w.addAdminTools(frameEl,
       connectCallback, messageCallback);
+    function leapCallback() {
+    }
+    function handleGesture(gesture) {
+      var now = Date.now();
+      if (!loaded) {
+        return;
+      }
+      if (!gesture || gesture.state !== 'start' || !gesture.direction) {
+        return;
+      }
+      if (now - lastGesture < 500) { // DEBOUNCE LIBRARY DID NOT WORK
+        return;
+      }
+      var swipeDirection;
+      lastGesture = now;
+      if (Math.abs(gesture.direction[0]) > Math.abs(gesture.direction[1])) {
+        swipeDirection = gesture.direction[0] > 0 ? SWIPE_RIGHT : SWIPE_LEFT;
+      } else {
+        swipeDirection = gesture.direction[1] > 0 ? SWIPE_UP : SWIPE_DOWN;
+      }
+      switch (swipeDirection) {
+        case SWIPE_LEFT:
+          cycleNext();
+          break;
+        case SWIPE_RIGHT:
+          cyclePrev();
+          break;
+        default:
+      }
+    }
     function connectCallback() {
       var contentEl = document.getElementById('my_content');
       ds.setBase(BASE);
